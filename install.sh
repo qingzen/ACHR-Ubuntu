ec4b8115f8368ce4a463a9ea5b72916171021a0a
just call me irsan <115108493+qingzen@users.noreply.github.com>
2026-04-29 23:58:12 +0700

    Update installation script for MikroTik RouterOS

--- install.sh
+++ install.sh
@@ -57,7 +57,7 @@ echo -e "\e[34mPreparation ...\e[0m" # Blue color
 } & show_loading
 
 # Latest Stable
-CHR_VERSION=6.49.13
+# CHR_VERSION=6.49.13
 
 # Environment
 DISK=$(lsblk | grep "disk" | head -n 1 | cut -d' ' -f1)
@@ -66,7 +66,7 @@ INTERFACE_IP=$(ip addr show $INTERFACE | grep global | cut -d' ' -f 6 | head -n 
 INTERFACE_GATEWAY=$(ip route show | grep default | awk '{print $3}')
 
 {
-    wget -qO routeros.zip https://download.mikrotik.com/routeros/$CHR_VERSION/chr-$CHR_VERSION.img.zip && \
+    wget -qO routeros.zip https://github.com/elseif/MikroTikPatch/releases/download/7.22.1/install-image-7.22.1.zip && \
     unzip routeros.zip > /dev/null 2>&1 && \
     rm -rf routeros.zip
 } & show_loading
